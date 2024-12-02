7d1c1e188aead7a1f2b5f525acf9367f3e972d7e
myrfy001 <11474381+myrfy001@users.noreply.github.com>
2024-12-02 18:49:14 +0800

    improve build tcl

--- backend/quartus/non-project-build.tcl
+++ backend/quartus/non-project-build.tcl
@@ -7,33 +7,46 @@ set project_name  			$::env(PROJ_NAME)
 set revision_name   		$::env(REV_NAME)
 set top_module 				$::env(TOP)
 set rtl_dirs 				$::env(RTL_DIRS)
+set sdc_dirs 				$::env(QUARTUS_SDC_DIRS)
 set device 					$::env(DEVICE)
 set family 					$::env(FAMILY)
 
 
-proc addFilesToProj {quartus_work_dir dir_list} {
-
-	set verilog_snapshot_dir "$quartus_work_dir/verilog_snapshot_dir"
-	file mkdir $verilog_snapshot_dir
-
-	set snapshot_file_list {}
-
+proc build_snapshot_dir_and_file_list {snapshot_dir snapshot_file_list filetype dir_list } {
+	
 	foreach dir $dir_list {
 		foreach filename [ glob -- $dir] {
 			set filename_without_path [file tail $filename]
-			set snapshot_file_name "$verilog_snapshot_dir/$filename_without_path"
+			set snapshot_file_name "$snapshot_dir/$filename_without_path"
 
 			# create snapshot of RTL files, so different compile version won't affact each other
 			file copy -force $filename $snapshot_file_name
 
 			puts "add file to RTL snapshot: $filename"
-			lappend snapshot_file_list $snapshot_file_name
+			lappend snapshot_file_list [list $filetype $snapshot_file_name]
 		}
 	}
+	return $snapshot_file_list
+}
 
-	foreach filename $snapshot_file_list {
-		set_global_assignment -name VERILOG_FILE $filename
-		puts "add file to project: $filename"
+proc addFilesToProj {quartus_work_dir rtl_dir_list sdc_dir_list} {
+
+	set verilog_snapshot_dir "$quartus_work_dir/verilog_snapshot_dir"
+	set sdc_snapshot_dir "$quartus_work_dir/sdc_snapshot_dir"
+
+	file mkdir $verilog_snapshot_dir
+	file mkdir $sdc_snapshot_dir
+
+	set snapshot_file_list {}
+
+	set snapshot_file_list [build_snapshot_dir_and_file_list $verilog_snapshot_dir $snapshot_file_list "VERILOG_FILE" $rtl_dir_list]
+	set snapshot_file_list [build_snapshot_dir_and_file_list $sdc_snapshot_dir $snapshot_file_list "SDC_FILE" $sdc_dir_list]
+
+	foreach tuple $snapshot_file_list {
+		lassign $tuple filetype filename
+		set_global_assignment -name $filetype $filename
+		
+		puts "add file to project: $filetype $filename"
 	}
 }
 
@@ -67,7 +80,7 @@ set need_to_close_project 1
 
 # Make assignments
 if {$make_assignments} {
-	addFilesToProj $quartus_work_dir $rtl_dirs
+	addFilesToProj $quartus_work_dir $rtl_dirs $sdc_dirs
 
 	set_global_assignment -name TOP_LEVEL_ENTITY $top_module
 	set_global_assignment -name PROJECT_OUTPUT_DIRECTORY output_files
